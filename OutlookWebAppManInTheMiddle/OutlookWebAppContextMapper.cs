66738782160b4d0db97be60b297a43260fe9f32e
Noah Surprenant <noah.surprenant@gmail.com>
2023-07-09 03:27:25 -0400

    Initial commit

---
+++ OutlookWebAppManInTheMiddle/OutlookWebAppContextMapper.cs
@@ -0,0 +1,41 @@
+﻿using HttpContextMapper;
+using System.Collections.Specialized;
+
+namespace OutlookWebAppManInTheMiddle
+{
+    public class OutlookWebAppContextMapper : ContextMapper
+    {
+        public OutlookWebAppContextMapper(IConfiguration config, IHttpClientFactory httpClientFactory, ILoggerFactory loggerFactory) : base(httpClientFactory, loggerFactory)
+        {
+            var target = config.GetValue<string>("TargetUrlWithProtocol");
+            if (target is null) throw new ArgumentNullException(nameof(target), "TargetUrlWithProtocol must be provided in configuration");
+
+            TargetUrlWithProtocol = target;
+            // TODO: fix how we are setting the builder
+            UriBuilder = new UriBuilder(TargetUrlWithProtocol);
+
+            DisableSetCookieEncoding = true;
+        }
+
+        protected override Task SetUriBuilderPathAndQuery(PathString path, NameValueCollection query)
+        {
+            if (HttpContext.Request.Query.ContainsKey("url"))
+            {
+                var urlParameter = query["url"];
+                query["url"] = ReplaceProxyWithTarget(ref urlParameter);
+            }
+            return base.SetUriBuilderPathAndQuery(path, query);
+        }
+
+        protected override Task FormRequestContent(Dictionary<string, string> formDictionary)
+        {
+            if (formDictionary.ContainsKey("destination"))
+            {
+                var destinationValue = formDictionary["destination"];
+                destinationValue = ReplaceProxyWithTarget(ref destinationValue);
+                formDictionary["destination"] = destinationValue;
+            }
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ OutlookWebAppManInTheMiddle/Program.cs
@@ -0,0 +1,44 @@
+
+using HttpContextMapper;
+
+namespace OutlookWebAppManInTheMiddle
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            builder.Services.RegisterDefaultReverseProxy();
+            builder.Services.AddScoped<IContextMapper, OutlookWebAppContextMapper>();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseMiddleware<ExceptionLoggerMiddleware>();
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+            app.UseAuthorization();
+
+            app.MapFallbackToContextMapper();
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
