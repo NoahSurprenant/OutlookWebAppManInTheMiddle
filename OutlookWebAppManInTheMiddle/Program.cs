8be95f699db1583b7eb0ec07281ecc944f3fb8ab
Noah Surprenant <noah.surprenant@gmail.com>
2023-07-31 23:42:51 -0400

    Add connectionstring to app settings. Add serilog. Add docker compose example. Add docker arm file

--- OutlookWebAppManInTheMiddle/Program.cs
+++ OutlookWebAppManInTheMiddle/Program.cs
@@ -2,6 +2,7 @@
 using HttpContextMapper;
 using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
+using Serilog;
 
 namespace OutlookWebAppManInTheMiddle
 {
@@ -9,55 +10,78 @@ namespace OutlookWebAppManInTheMiddle
     {
         public static void Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .CreateLogger();
 
-            // Add services to the container.
-
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-
-            builder.Services.RegisterDefaultReverseProxy();
-            builder.Services.AddScoped<IContextMapper, OutlookWebAppContextMapper>();
-
-            builder.Services.AddDbContextFactory<OutlookWebAppDbContext>(options =>
+            try
             {
-                var connectionString = builder.Configuration.GetConnectionString("Database");
+                Log.Information("Starting application");
 
-                var sqliteBuilder = new SqliteConnectionStringBuilder(connectionString);
+                var builder = WebApplication.CreateBuilder(args);
 
-                sqliteBuilder.DataSource = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, sqliteBuilder.DataSource));
+                builder.Host.UseSerilog();
 
-                connectionString = sqliteBuilder.ToString();
+                // Add services to the container.
 
-                options.UseSqlite(connectionString);
-            });
+                builder.Services.AddControllers();
+                // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+                builder.Services.AddEndpointsApiExplorer();
+                builder.Services.AddSwaggerGen();
 
-            var app = builder.Build();
+                builder.Services.RegisterDefaultReverseProxy();
+                builder.Services.AddScoped<IContextMapper, OutlookWebAppContextMapper>();
 
-            var factory = app.Services.GetRequiredService<IDbContextFactory<OutlookWebAppDbContext>>();
-            var context = factory.CreateDbContext();
-            context.Database.Migrate();
+                builder.Services.AddDbContextFactory<OutlookWebAppDbContext>(options =>
+                {
+                    var connectionString = builder.Configuration.GetConnectionString("Database");
 
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
+                    var sqliteBuilder = new SqliteConnectionStringBuilder(connectionString);
+
+                    sqliteBuilder.DataSource = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, sqliteBuilder.DataSource));
+
+                    connectionString = sqliteBuilder.ToString();
+
+                    Log.Logger.Information("ConnectionString: {ConnectionString}", connectionString);
+
+                    options.UseSqlite(connectionString);
+                });
+
+                var app = builder.Build();
+
+                var factory = app.Services.GetRequiredService<IDbContextFactory<OutlookWebAppDbContext>>();
+                var context = factory.CreateDbContext();
+                context.Database.Migrate();
+
+                // Configure the HTTP request pipeline.
+                if (app.Environment.IsDevelopment())
+                {
+                    app.UseSwagger();
+                    app.UseSwaggerUI();
+                }
+
+                app.UseMiddleware<ExceptionLoggerMiddleware>();
+
+                app.UseHttpsRedirection();
+
+                app.UseRouting();
+                app.UseAuthorization();
+
+                app.MapFallbackToContextMapper();
+                app.MapControllers();
+
+                app.Run();
+            }
+            catch (Exception ex)
             {
-                app.UseSwagger();
-                app.UseSwaggerUI();
+                Log.Fatal(ex, "Application terminated unexpectedly");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
             }
 
-            app.UseMiddleware<ExceptionLoggerMiddleware>();
-
-            app.UseHttpsRedirection();
-
-            app.UseRouting();
-            app.UseAuthorization();
-
-            app.MapFallbackToContextMapper();
-            app.MapControllers();
-
-            app.Run();
+            
         }
     }
 }
